bcdd6c32c9c1c2833c0b864d4da91c6df0bf99f6
shruthi-1 <123851514+shruthi-1@users.noreply.github.com>
2025-05-26 09:17:46 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,7 +7,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 import re
 import secrets
-import openai
 import os
 import time
 from dotenv import load_dotenv
